496d63d914f4b6baed952bfcf5b93c8529e69d68
NaFl <72364452+nflahza@users.noreply.github.com>
2024-07-08 10:14:29 +0800

    update pt3

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const config = {
+  // Use the basePath for GitHub Pages if necessary
+  // basePath: '/your-repo-name',
+  // Use the assetPrefix for GitHub Pages if necessary
+  // assetPrefix: '/your-repo-name/',
+  // Specify exportPathMap to export your dynamic pages
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/cognitive": { page: "/cognitive" },
+      "/quantitative": { page: "/quantitative" },
+      "/literature_a": { page: "/literature_a" },
+      "/literature_b": { page: "/literature_b" },
+    };
+  },
+};
 
-export default nextConfig;
+export default config;
